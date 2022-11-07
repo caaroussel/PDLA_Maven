42168d2c98b3938b8868aa997ae6cea26f426b70
Francis <caa.roussel@gmail.com>
2022-11-07 15:59:26 +0100

    first commit

---
+++ src/main/java/LoginVerify.java
@@ -0,0 +1,48 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class LoginVerify
+ */
+
+@WebServlet("/LoginVerify")
+
+public class LoginVerify extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginVerify() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect("log.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uname=request.getParameter("uname");
+		String pwd=request.getParameter("password");
+		if (uname.equals("admin")&& pwd.equals("admin")) {
+			System.out.println("les identifiants sont corrects");
+		}else
+			 System.out.println("Erreur dans les identifiants");
+	}
+
+}
